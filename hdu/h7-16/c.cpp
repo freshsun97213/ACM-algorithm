9ae36c3e67010bc7d48a6d09bc561d007a89db29
ironwaste <s691195466@outlook.com>
2023-08-30 15:53:34 +0800

    change the file by ubuntu

---
+++ hdu/h7-16/c.cpp
@@ -0,0 +1,18 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int n, a, b;
+  while (~scanf("%d %d", &a, &b)) {
+    if (a + b == 0) {
+      break;
+    } // 在这种情况下，也就是在～scanf加while的情况下
+      // while后无东西break等效于return
+    cout << a + b << endl;
+  }
+
+  return 0;
+}
