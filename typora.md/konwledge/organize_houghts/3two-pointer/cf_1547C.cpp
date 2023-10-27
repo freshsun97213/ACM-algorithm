c440c1d04356e095c9b68591aa07c5b525107b29
sd <s691195466@outlook.com>
2023-10-27 18:59:17 +0800

    two pointers

--- typora.md/konwledge/organize_houghts/3two-pointer/cf_1547C.cpp
+++ typora.md/konwledge/organize_houghts/3two-pointer/cf_1547C.cpp
@@ -10,52 +10,36 @@ void solve(){
     vi a(n),b(m),c;
     for(int i =0 ;i < n ;i ++){cin >> a[i];}
     
-    for(int i =0 ;i < n ;i ++){cin >> b[i];}
+    for(int i =0 ;i < m ;i ++){cin >> b[i];}
     int i =0 , j=0;
-    while(ok && (i < n || j < m)){
-        ok = 1;
-        while(i < n){
-            if(!a[i]){
-                k ++;
-                ok ++;
-                c.push_back(a[i++]);
-            }else if(a[i]<= k){
-                c.push_back(a[i++]);
-            }else{
-                if(ok == 1){
-                    ok = 0;
-                    break;
-                }
-            }
-        }
-        while(j < m){
-            if(!b[j]){
-                k ++;
-                ok ++;
-                c.push_back(b[j++]);
-            }else if(b[j]<= k){
-                c.push_back(b[j++]);
-            }else{
-                if(!ok){
-                    ok = 0;
-                    break;
-                }
-            }
-        }
-        if(!ok){
-            cout << "-1" <<endl;
-            return ;
+    while(i != n || j != m){
+        if(i != n && a[i] == 0){
+            c.push_back(0);
+            k ++;
+            i ++;
+        }else if(i!=n&& a[i] <= k){
+            c.push_back(a[i]);
+            i ++;
+        }else if(j != m&& !b[j] ){
+            c.push_back(0);
+            j ++;
+            k ++;
+        }else if(j != m && b[j] <=k){
+            c.push_back(b[j]);
+            j ++;
+        }else{
+            cout << -1 <<'\n';
+            ok = 0;
+            break;
         }
     }
     if(ok){
         for(auto x : c){
-            cout << x <<" ";
-        }  
-        cout << '\n'; 
+            cout << x << " ";
+        }
+        cout <<'\n';
+        // c.clear();
     }
-
-
-
     return ;
 }
 
@@ -69,3 +53,40 @@ int main(){
     }
     return 0;
 }
+
+/*      双指针里面 最好不要两个while循环嵌套
+        因为里面和外面的循环 可能会导致 各种边界条件
+        非常难调试
+*/
+// void solve(){
+//     vi a,b,c;
+//     int ok = 1,k,n,m,i = 0,j = 0;
+//      输入
+//     i = 0;
+//     while(i + j < n + m && ok){
+//         if(i < a.size()&&!a[i]){
+//             k ++;
+//             c.push_back(a[i++]);
+//         }else if(i < a.size() && a[i] <= k){
+//             c.push_back(a[i++]);
+//         }else{
+//             if(j < m && ok){
+//             while(j < m && ok){
+//                 if(j < b.size() && !b[j]){
+//                     k ++;
+//                     c.push_back(b[j++]);
+//                 }else if(j < b.size() && b[j] <= k){
+//                     // k ++;
+//                     c.push_back(b[j++]);
+//                 }else{
+//                     ok = 0;
+//                     break;
+//                 }
+//             }
+//             }else{
+//                 ok = 0;
+//             }
+//             if(!ok){break;}
+//         }
+//     }
+
