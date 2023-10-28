91f3556d5e59357f164f4c29336b686fc1ce1d84
sd <s691195466@outlook.com>
2023-10-28 11:17:27 +0800

    two pointers

---
+++ typora.md/konwledge/organize_houghts/3two-pointer/cf_1669F.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+
+#define i64 long long
+
+#define vi vector<int>
+using namespace std;
+
+
+void solve(){
+
+    int n;
+    cin >> n;
+    vi w(n);
+    for(int i = 0;i < n ;i ++)
+        cin >> w[i];
+
+    int r =n-1 ,l= 0,wr=0,wl=0;
+    i64 sum = 0;
+    while(r >= l){
+        
+        if(wr <= wl){
+            wr += w[r];
+            r --;
+        if(wr == wl){
+            sum = n-r-1 + l;
+        }
+        }else if(wr > wl){
+            wl += w[l];
+            l ++;
+        if(wr == wl){
+            sum = n-r-1 + l;
+        }
+        }
+    }
+
+    cout << sum << '\n';
+}
+
+
+
+int main(){
+    int t ;
+    cin >> t;
+    while(t --){
+        solve();
+    }
+    return 0;
+}
